9a59f942c46f5d7e6fc7acd9a10ba3faa61475e1
engelbert teh <engelbertteh.dev@gmail.com>
2024-11-13 19:26:52 +0800

    fix: removed unused keywords

--- src/app/service/(dao)/db-service.ts
+++ src/app/service/(dao)/db-service.ts
@@ -1,10 +1,11 @@
-import sql from '@/app/service/db';
-import { createSupabase } from '@/app/service/db-conn-service';
+// import sql from '@/app/service/db';
+// import { createSupabase } from '@/app/service/db-conn-service';
 
 export async function proceduralCall() {
   // make supabase db call
   //   const supabase = createSupabase();
-  const supabase = await createSupabase();
+  // const supabase = await createSupabase();
 
-  return await sql`SELECT * FROM Employee`;
+  // return await sql`SELECT * FROM Employee`;
+  return null;
 }
